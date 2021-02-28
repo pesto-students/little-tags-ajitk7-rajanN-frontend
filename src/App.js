02b136a1ef8d26c390c9ebd3ffa0a9932d37f534
ajitk7 <ajit.kulkarni167@gmail.com>
2021-02-28 22:57:58 +0530

    footer component added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import Header from './components/header/header.component.jsx';
+import Header from './components/header/header.component';
+import Footer from './components/footer/footer.component';
 import SignInAndSingUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+
 
 function App() {
   return (
@@ -20,7 +17,7 @@ function App() {
           <Route exact path='/' component={HomePage} />
           <Route path='/signin' component={SignInAndSingUpPage} />
         </Switch>
-     
+     <Footer/>
     </div>
   );
 }
